1266a2acbfa9c44124b49566fef616d938164edf
Sumayya Akhter <sumaiya.akter.2894@gmail.com>
2026-02-24 15:09:39 +0600

    Add home page with latest and popular inventories

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,35 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using InventoryApp.Models;
 
 namespace InventoryApp.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly AppDbContext _db;
+
+    public HomeController(AppDbContext db)
     {
+        _db = db;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var latest = await _db.Inventories
+            .Include(i => i.Creator)
+            .OrderByDescending(i => i.CreatedAt)
+            .Take(10)
+            .ToListAsync();
+
+        var popular = await _db.Inventories
+            .Include(i => i.Creator)
+            .OrderByDescending(i => i.Items.Count)
+            .Take(5)
+            .ToListAsync();
+
+        ViewBag.Latest = latest;
+        ViewBag.Popular = popular;
         return View();
     }
 
