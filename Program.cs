069701abca850f31065c508371e9fd2e1117dd31
Sumayya Akhter <sumaiya.akter.2894@gmail.com>
2026-03-07 21:34:18 +0600

    Fix port binding for Render

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddAntiforgery(options => {
     options.Cookie.SameSite = SameSiteMode.Lax;
 });
